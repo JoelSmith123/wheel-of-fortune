623dce385ee706f5369abafb80667d5c49106273
Joel Smith <joelwsmith123@gmail.com>
2018-10-13 23:17:19 -0600

    Add player names to game

---
+++ domUpdates.js
@@ -0,0 +1,52 @@
+const playerNameInput = $('.player-name-input')
+const player1NameDisplay = $('.welcome-page-player1-name')
+const player2NameDisplay = $('.welcome-page-player2-name')
+const player3NameDisplay = $('.welcome-page-player3-name')
+const submitPlayersButtonIcon = $('.fa-check-circle')
+const startGameButtonText = $('.start-game-button-text')
+const playersNamesArr = []
+let game
+
+const domUpdates = {
+  
+  submitPlayersName(e) {
+    e.preventDefault()
+
+    if (playersNamesArr.length === 0) {
+      game = new Game()
+      acceptNewPlayerName(player1NameDisplay, 0)
+    } else if (playersNamesArr.length === 1) { 
+      acceptNewPlayerName(player2NameDisplay, 1)
+    } else if (playersNamesArr.length === 2) {
+      acceptNewPlayerName(player3NameDisplay, 2)
+      changeButtonSubmitToStart()
+    } else if (playersNamesArr.length === 3) {
+      changeGameDisplay()
+    }
+  }
+
+}
+
+function acceptNewPlayerName(player, playerIndex) {
+  player.text(playerNameInput.val())
+  playersNamesArr.push(playerNameInput.val())
+  game.players[playerIndex].name = playerNameInput.val()
+  playerNameInput.val('')
+}
+
+function changeButtonSubmitToStart() {
+  submitPlayersButtonIcon.remove()
+  submitPlayersButton.addClass('start-game-button')
+  startGameButtonText.removeClass('display-mode-none')
+}
+
+function changeGameDisplay() {
+  welcomePage.addClass('display-mode-none')
+  mainGameDisplay.removeClass('display-mode-none')
+}
+
+
+if (typeof module !== 'undefined') {
+  module.exports = domUpdates;
+}
+
--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-let welcomePage = $('.welcome-page')
-let mainGameDisplay = $('.main-game-display')
-let submitPlayersButton = $('.submit-players-button')
+const welcomePage = $('.welcome-page')
+const mainGameDisplay = $('.main-game-display')
+const submitPlayersButton = $('.submit-players-button')
+
+playerNameInput.attr('autocomplete', 'off')
+submitPlayersButton.remove('start-game-button-text')     
 
 
-submitPlayersButton.on('click', changeGameDisplay)
+submitPlayersButton.on('click', domUpdates.submitPlayersName)
 
 
-function changeGameDisplay(e) {
-  e.preventDefault()
-  welcomePage.addClass('display-mode-none')
-  mainGameDisplay.removeClass('display-mode-none')
-}
-
-// submitPlayersButton on click
-//    if a name hasnt been entered yet 
-//        start new game, keep on welcome screen
-//        assign input to property of player
-//    else
-//        assign input to next property of player that isnt undefined
-//    if all players arent undefined
-//        maybe change the input accept button to a start game button
-//        hide the welcome page, display the game page
