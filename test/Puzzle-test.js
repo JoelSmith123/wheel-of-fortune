28ab700d199ad373617d266d47549d8d5ed85039
Joel Smith <joelwsmith123@gmail.com>
2018-10-17 21:20:03 -0600

    Add beginning test structure for each test needed

--- test/Puzzle-test.js
+++ test/Puzzle-test.js
@@ -2,25 +2,13 @@ const { expect } = require('Chai')
 const Puzzle = require('../lib/Puzzle')
 const Data = require('../lib/data')
 
-describe.only('Puzzle', () => {
+describe('Puzzle', () => {
   var puzzle
   beforeEach( () => {
     puzzle = new Puzzle();
   })
 
-  it('should check if user guess letter is contained in the correct answer', () => {
-    puzzle.currentPuzzle = {  
-          category: 'The 90s',
-          number_of_words: 1,
-          total_number_of_letters: 7,
-          first_word: 7, 
-          description:'Puzzles pertaining to the decade in question.',
-          correct_answer: 'Beepers',
-        };
-    expect(puzzle.checkLetters('e')).to.equal(true)
-  })
-
-  it('should compare solve attempt input to correct answer', () => {
+  it.skip('should compare solve attempt input to correct answer', () => {
     puzzle.currentPuzzle = {  
           category: 'The 90s',
           number_of_words: 1,
--- test/Round-test.js
+++ test/Round-test.js
@@ -4,17 +4,27 @@ const puzzle = require('../lib/Puzzle')
 const Data = require('../lib/data')
 
 
-
 describe('Round', () => {
-  // var round
-  // beforeEach( () => {
-  //   round = new Round()
-  // })
+  var round
+  beforeEach( () => {
+    round = new Round()
+  })
 
-  it('should fetch a random puzzle from our dataset', () => {
-    var round = new Round()
+  it.skip('should fetch a random puzzle from our dataset', () => {
     round.getNewPuzzle()
     expect(round.getNewPuzzle()).to.not.be.undefined;
+  })
+
+  it.skip('should change green tiles to white tiles for each defined letter', () => {
+    round.findCorrectTile(array, columnNum)
+  })
+
+  it.skip('should change board tile colors based on the length of the puzzle', () => {
+    round.changeTileColor(correctAnswer)
+  })
+
+  it.skip('should add guessed letters to the lettersGuessed array for tracking', () => {
+    round.checkGuessedLetters(letter)
   })
 
 })
